6da0789406370fb51c6640b9f825ce51dedb3d27
Richard Peltonen <richard.peltonen@cgi.com>
2019-04-23 19:41:06 +0300

    use switch with router

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom'
 import Header from './components/header/header';
 import Main from './pages/main/main';
 import Comments from './pages/comments/comments';
@@ -10,6 +10,7 @@ import * as serviceWorker from './serviceWorker';
 // TODO: The app currently has no high level state management (meaning the Main components state will reset on navigation)
 // to fix this we should implement redux or
 // introduce higher root level component to hold the state
+// TODO: Use the es6 class syntax here also since its used everywhere else
 const routing = (
     //TODO: Refactor Router to router.tsx
     <Router>
@@ -17,8 +18,11 @@ const routing = (
             <div className="app__container">
                 <Header />
                 <div className="app__content">
-                    <Route exact path="/" component={Main} />
-                    <Route path="/comments/" component={Comments} />
+                {/* display only a single route to be rendered amongst the several defined routes */}
+                    <Switch>
+                        <Route exact path="/" component={Main} />
+                        <Route path="/comments/" component={Comments} />
+                    </Switch>
                 </div>
             </div>
         </div>
